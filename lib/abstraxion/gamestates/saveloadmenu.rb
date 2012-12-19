bdba0d3d5bc28a07f3a8088ae0bce4cebc480c19
jamesshieh <randomletters13@gmail.com>
2012-12-19 18:25:01 -0500

    reorganize saveload states

--- lib/abstraxion/gamestates/saveloadmenu.rb
+++ lib/abstraxion/gamestates/saveloadmenu.rb
@@ -12,9 +12,20 @@ module Abstraxion
       @size = FACTOR * scale
       $node_size = NODE_SIZE * scale
     end
-    # Draw the 3 save boxes
-    def draw
+
+    # Load the screen and previews
+    def setup
       super
+      clear_towers
+      load_previews
+      @savedt1, @savedt2, @savedt3 = @saved_towers.values
+      draw_cursor
+      @save1 ||= SaveSlot.create(:x => WINDOW_W/2, :y => 2 * WINDOW_H/7)
+      draw_tower(@savedt1, 255, 2 * WINDOW_H/7 - $node_size * @savedt1.y / 2, @size) unless @savedt1.nil?
+      @save2 ||= SaveSlot.create(:x => WINDOW_W/2, :y => 4 * WINDOW_H/7)
+      draw_tower(@savedt2, 255, 4 * WINDOW_H/7 - $node_size * @savedt2.y / 2, @size) unless @savedt2.nil?
+      @save3 ||= SaveSlot.create(:x => WINDOW_W/2, :y => 6 * WINDOW_H/7)
+      draw_tower(@savedt3, 255, 6 * WINDOW_H/7 - $node_size * @savedt3.y / 2, @size) unless @savedt3.nil?
     end
   end
 end
--- lib/abstraxion/helpers/saveloadhelper.rb
+++ lib/abstraxion/helpers/saveloadhelper.rb
@@ -1,18 +1,4 @@
 module SaveLoadHelper
-  # Load the screen and previews
-  def setup
-    super
-    clear_towers
-    load_previews
-    @savedt1, @savedt2, @savedt3 = @saved_towers.values
-    draw_cursor
-    @save1 ||= SaveSlot.create(:x => WINDOW_W/2, :y => 2 * WINDOW_H/7)
-    draw_tower(@savedt1, 255, 2 * WINDOW_H/7 - $node_size * @savedt1.y / 2, @size) unless @savedt1.nil?
-    @save2 ||= SaveSlot.create(:x => WINDOW_W/2, :y => 4 * WINDOW_H/7)
-    draw_tower(@savedt2, 255, 4 * WINDOW_H/7 - $node_size * @savedt2.y / 2, @size) unless @savedt2.nil?
-    @save3 ||= SaveSlot.create(:x => WINDOW_W/2, :y => 6 * WINDOW_H/7)
-    draw_tower(@savedt3, 255, 6 * WINDOW_H/7 - $node_size * @savedt3.y / 2, @size) unless @savedt3.nil?
-  end
 
   # Saves state based on mouse hover position
   def save_state
