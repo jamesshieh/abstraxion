e2c9d69dd7514d5e7eab3b085c1b0ee217f82687
jamesshieh <randomletters13@gmail.com>
2012-12-20 16:49:23 -0500

    zorder fixes

--- lib/abstraxion/objects/cursor.rb
+++ lib/abstraxion/objects/cursor.rb
@@ -3,6 +3,7 @@ module Abstraxion
     def initialize(options = {})
       super(options.merge(:image => Image["cursor.png"]))
       self.zorder = ZOrder::Cursor
+      @zorder = 1000
     end
     def update
       @x = $window.mouse_x + 6
