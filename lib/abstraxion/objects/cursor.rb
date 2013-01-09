429c8ab8bda7aaba8ef869c53b5125d3662a3ca0
jamesshieh <randomletters13@gmail.com>
2013-01-08 22:46:26 -0800

    removed obsolete line of zorder for cursor

--- lib/abstraxion/objects/cursor.rb
+++ lib/abstraxion/objects/cursor.rb
@@ -3,7 +3,6 @@ module Abstraxion
     def initialize(options = {})
       self.zorder = ZOrder::Cursor
       super(options.merge(:image => Image["cursor.png"]))
-      @zorder = 1000
     end
     def update
       @x = $window.mouse_x + 6
